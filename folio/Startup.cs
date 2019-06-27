460473e463bdd2c3e6311d1d55d9e5be228f6e3c
Zhu Zhan Yan <program.nom@gmail.com>
2019-06-27 20:27:09 +0800

    Added MVC support into Startup.cs

--- folio/Startup.cs
+++ folio/Startup.cs
@@ -11,23 +11,25 @@ namespace folio
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // support for MVC
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            // shown exception page if development uild
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            // serve static files in wwwroot directory
+            app.UseStaticFiles();
+            // define default MVC route: controller/action/id
+            app.UseMvc((routes) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
