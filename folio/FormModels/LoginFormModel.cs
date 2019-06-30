b3324ee0ea8fb1a944430a6c6c3c94536b8f1d40
Zhu Zhan Yan <program.nom@gmail.com>
2019-06-30 15:08:42 +0800

    Added LoginFormModel to define the params required to login

---
+++ folio/FormModels/LoginFormModel.cs
@@ -0,0 +1,14 @@
+/*
+ * NP Web Assignment
+ * Login Form Model
+*/
+
+namespace folio.FormModels
+{
+    // model defines the properties required to login
+    public class LoginFormModel
+    {
+        public string EmailAddr  { get; set; }
+        public string Password  { get; set; }
+    }
+}
