443b117dda31b2ff92606e4aec833995eea94ecd
Zhu Zhan Yan <program.nom@gmail.com>
2019-06-30 09:53:55 +0800

    Added missing using reference in IContentService

--- folio/Services/Content/IContentService.cs
+++ folio/Services/Content/IContentService.cs
@@ -3,16 +3,18 @@
  * Web Assignment 1
  */
 
+using System.IO;
+
 namespace folio.Services.Content
 {
     public interface IContentService
     {
         // Upload the content in the given contentStream to the content service
         // returns a content id string which can be used to retrieve the content 
-        public string UploadContent(Stream contentStream);
+        string UploadContent(Stream contentStream);
 
         // Decode the url that can be used to retrieve the content given the 
         // content id returned by UploadContent()
-        public string decodeUrl(string contentId);
+        string decodeUrl(string contentId);
     }
 }
