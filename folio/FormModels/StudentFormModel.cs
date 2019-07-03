c6280edf30ea4e72b1c82779a53e0e1b758c620b
Zhu Zhan Yan <program.nom@gmail.com>
2019-07-03 20:10:37 +0800

    Prevent students from changing their emails during update

--- folio/FormModels/StudentFormModel.cs
+++ folio/FormModels/StudentFormModel.cs
@@ -17,18 +17,20 @@ namespace folio.FormModels
         public string EmailAddr { get; set; }
         public string Password { get; set; }
         public int MentorId { get; set; }
-
-        public void Apply(Student student)
+        
+        // Apply the properties in the student form model 
+        // given method apply: "create" or "update"
+        public void Apply(Student student, string method="update")
         {
-            student.Name = this.Name;
-            student.Course = this.Course;
-            student.Photo = this.Photo;
-            student.Description = this.Description;
-            student.Achievement = this.Achievement;
-            student.ExternalLink = this.ExternalLink;
-            student.EmailAddr = this.EmailAddr;
-            student.Password = this.Password;
-            student.MentorId = this.MentorId;
+            if(this.Name != null) student.Name = this.Name;
+            if(this.Course != null) student.Course = this.Course;
+            if(this.Photo != null) student.Photo = this.Photo;
+            if(this.Description != null) student.Description = this.Description;
+            if(this.Achievement != null) student.Achievement = this.Achievement;
+            if(this.ExternalLink != null) student.ExternalLink = this.ExternalLink;
+            if(method == "create" && this.EmailAddr != null) student.EmailAddr = this.EmailAddr;
+            if(this.Password != null) student.Password = this.Password;
+            if(this.MentorId != null) student.MentorId = this.MentorId;
         }
     }
 }
