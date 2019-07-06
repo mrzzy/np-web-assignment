cdaed754c188f8ead9b9cef6543831d67fd82c02
Zhu Zhan Yan <program.nom@gmail.com>
2019-07-06 15:18:48 +0800

    Added APITest to test if API can be accessed

---
+++ folio_tests/API/APITests.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using System;
+using DotNetEnv;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace folio.Tests.API
+{
+    public class APITest
+    {
+        /* API intergration tests */
+        [Fact]
+        public async void TestAccessAPI()
+        {
+            // load api host form host
+            DotNetEnv.Env.Load();
+            string apiHost = Environment.GetEnvironmentVariable("API_HOST");
+            
+            // Attempt to access the api at the /api/students route
+            string apiUrl = "http://" + apiHost + "/api/students";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client
+                .GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
