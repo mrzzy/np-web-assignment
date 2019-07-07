1636e8c4c661672a11a5308e01c37852144c9e04
Zhu Zhan Yan <program.nom@gmail.com>
2019-07-07 12:54:29 +0800

    Configure webpack to output folio_ui/wwwroot
    Addtionality configured support in webpack for
    - es6 javascript support with babel-loader
    - css injection with style-loader & css-loader
    - copying images to wwwroot with file-loader

---
+++ folio_ui/Views/webpack.config.js
@@ -0,0 +1,47 @@
+/*
+ * webpack configuration
+ * NP Web Assignment
+*/
+
+const path = require("path");
+
+module.exports = {
+    // client scripts point 
+    entry: path.resolve(__dirname, "index.js"),
+    // output configuration
+    output: {
+        path: path.resolve(__dirname, "..", "wwwroot"),
+        publicPath: '/',
+        filename: path.join("js", "bundle.js")
+    },
+    // webpack module
+    module: {
+        rules: [
+            // babel
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [ 'babel-loader' ]
+            },
+            // css
+            { 
+                test: /\.css$/,
+                use: [ 'style-loader','css-loader' ]
+            },
+            // images
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name:'[name].[ext]',
+                        outputPath: "img"
+                    },
+                }],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".json"]
+    }
+};
