04562d6046634b5b1225d8ff74de4437b910566f
Zhu Zhan Yan <program.nom@gmail.com>
2019-07-15 23:39:56 +0800

    Added Login View js to perform login with user credientials

--- folio_ui/Views/Auth/Login.js
+++ folio_ui/Views/Auth/Login.js
@@ -3,5 +3,23 @@
  * JS
 */
 
-// login view styling
-import "./Login.css"
+import "./Login.css";
+import Auth from "../Auth.js";
+
+// trigger form submit on button click 
+$("#login-btn-submit").click(() => {
+    $("#login.form").submit();
+});
+
+// submit the login form to perform login 
+$("#login-form").submit((event) => {
+    event.preventDefault();
+
+    // collect login credientials from form
+    const emailAddr = $("#login-EmailAddr").val();
+    const password = $("#login-Password").val();
+
+    // perform login
+    const auth = new Auth("http://" + process.env.API_INGRESS);
+    auth.login(emailAddr, password);
+});
