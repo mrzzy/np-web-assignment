940364b1d71396d43fc67e32e6189d20e8ba47b5
Zhu Zhan Yan <program.nom@gmail.com>
2019-07-17 21:45:43 +0800

    Added StudentController's Portfolio() action to render student portfolios

--- folio_ui/Controllers/StudentController.cs
+++ folio_ui/Controllers/StudentController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * NP Web Assignment
+ * Folio UI
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -7,10 +12,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using folio.Models;
+using folio.Services.API;
 
 namespace folio_ui.Controllers
-{
+{     
+    // ui controller: /student
     public class StudentController : Controller
     {
+        // display student portfolio for the given id
+        [HttpGet("/student/portfolio/{id}")]
+        public ActionResult Portfolio(int id)
+        {
+            // pull student portfolio data for id
+            APIClient apiClient = new APIClient();
+            string portfolioJson = apiClient .CallAPI("GET", 
+                    "/api/student/portfolio/" + id);
+            Student student = JsonConvert.DeserializeObject<Student>(portfolioJson);
+            return View(student);
+        }
     }
 }
