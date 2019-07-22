a883dcaaa32ffac0764e50194f6d7e0572881e47
Jun Wei Loh <s10186270@connect.np.edu.sg>
2019-07-22 11:00:09 +0800

    Added SkillsetController to views

---
+++ folio_ui/Controllers/SkillsetController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using folio.Models;
+using System.Net;
+
+namespace folio_ui.Controllers
+{
+    public class SkillsetController : Controller
+    {
+        // GET: Skillset
+        public async Task<ActionResult> Index(int id)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5000");
+            HttpResponseMessage response = await client.GetAsync("/api/skillset/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                List<SkillSet> skillSetList = JsonConvert.DeserializeObject<List<SkillSet>>(data);
+                return View(skillSetList);
+            }
+            else
+            {
+                return View(new List<SkillSet>());
+            }
+        }
+    }
+}
